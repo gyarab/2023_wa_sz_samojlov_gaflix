498033f75aad69f1f0013c10f8a7f5f86c009499
Vladimír Samojlov <vladimir.samojlov@student.gyarab.cz>
2024-04-26 09:30:54 +0200

    26.04.2024

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from filmy.models import Movie
+from filmy.models import Movie, Director, Genre
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
     pass
 
+class DirectorAdmin(admin.ModelAdmin):
+    pass
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Movie, MovieAdmin)
 
+admin.site.register(Director, DirectorAdmin)
+
+admin.site.register(Genre, GenreAdmin)
--- filmy/models.py
+++ filmy/models.py
@@ -2,10 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    title = models.CharField(max_length = 300)
+    name = models.CharField(max_length = 300)
     year = models.PositiveSmallIntegerField(blank = True, null = True)
     footage = models.PositiveSmallIntegerField(blank = True, null = True, help_text="in minutes")
 
     def __str__(self):
-        return self.title
+        return self.name
     
+class Director(models.Model):
+    name = models.CharField(max_length = 300)
+    birth_year = models.PositiveSmallIntegerField(blank = True, null = True)
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null = True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length = 300)
