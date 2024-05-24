f4784bf2f6448870d53004c357865f0a6d1ba9bd
Vladimír Samojlov <vladimir.samojlov@student.gyarab.cz>
2024-05-24 09:31:40 +0200

    seznamy v tabulkach DB

--- filmy/admin.py
+++ filmy/admin.py
@@ -3,6 +3,18 @@ from filmy.models import Movie, Director, Genre, Actor
 
 # Register your models here.
 class MovieAdmin(admin.ModelAdmin):
+
+    ## zobrazeni listu v tabulce Movie
+    list_display = ['id', 'name', 'year', 'footage', 'genres_display', 'director']
+
+    ## odkazy v seznamu, vyber polozek ktere budou klikaci
+    list_display_links = ['id', 'name']
+
+    ## moznost hledani v seznamu
+    search_fields = ['name', 'director_name']
+
+    ## filtr seznamu
+    list_filter = ['genres', 'year']
     pass
 
 class DirectorAdmin(admin.ModelAdmin):
--- filmy/models.py
+++ filmy/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
-# Create your models here.
 class Movie(models.Model):
     name = models.CharField(max_length = 300)
     year = models.PositiveSmallIntegerField(blank = True, null = True)
     footage = models.PositiveSmallIntegerField(blank = True, null = True, help_text="in minutes")
+    description = models.TextField(blank=True)
+    main_picture = models.ImageField(blank=True, null = True)
+
+    ## vyber jednoho rezisera, jedna se o klic
+    director = models.ForeignKey('Director', blank=True, null=True, on_delete=models.SET_NULL)
+
+    ## odkaz na tridu Actor, moznost vybrat herce z DB
+    actors = models.ManyToManyField('Actor', blank=True)
+
+    genres = models.ManyToManyField('Genre', blank=True)
 
     def __str__(self):
         return self.name
+
+    def genres_display(self):
+
+        return ", ".join([i.name for i in self.genres.all()])
     
 class Director(models.Model):
     name = models.CharField(max_length = 300)
@@ -15,11 +28,20 @@ class Director(models.Model):
     description = models.TextField(blank=True)
     main_picture = models.ImageField(blank=True, null = True)
 
+    def __str__(self):
+        return self.name
+
 class Actor(models.Model):
     name = models.CharField(max_length = 300)
     birth_year = models.PositiveSmallIntegerField(blank = True, null = True)
     description = models.TextField(blank=True)
     main_picture = models.ImageField(blank=True, null = True)
 
+    def __str__(self):
+        return self.name
+
 class Genre(models.Model):
     name = models.CharField(max_length = 300)
+
+    def __str__(self):
+        return self.name
