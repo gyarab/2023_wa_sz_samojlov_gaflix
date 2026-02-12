6d0304ef516b315243176f6c75816ee8d4712b0d
Vlada45 <94285852+Vlada45@users.noreply.github.com>
2026-02-12 20:56:57 +0100

    vercel fix

--- app/gaflix/wsgi.py
+++ app/gaflix/wsgi.py
@@ -8,11 +8,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from pathlib import Path
+from dotenv import load_dotenv
 
-from django.core.wsgi import get_wsgi_application
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Allow Python to find the gaflix module
+sys.path.append(str(BASE_DIR))
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gaflix.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
 app = application
